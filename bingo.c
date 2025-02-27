81fc938c761a66364facc12f0247c48b3d669ac5
thassyodavid <113135589+thassyodavid@users.noreply.github.com>
2025-02-27 15:55:07 -0300

    Add files via upload

---
+++ bingo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//Exercício The Huxley | Bingo - Acertos 4 de 4
+
+int main() {
+    int numero;
+    scanf("%d",&numero);
+    printf("O numero sorteado foi: %d\n",numero);
+	return 0;
+}
